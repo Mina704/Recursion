3a80aae102293ca9660c91bf5177f363b732cee3
Mina704 <144085944+Mina704@users.noreply.github.com>
2026-02-28 18:32:04 +0200

    Create Base Converssion.cpp

---
+++ Base Converssion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void toBinary(long long n){
+    if(n == 0)
+        return;
+    toBinary(n / 2);
+    cout << n % 2;
+}
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        long long n;
+        cin >> n;
+        if(n == 0) cout << 0;
+        else toBinary(n);
+        cout << endl;
+    }
+    return 0;
+}
